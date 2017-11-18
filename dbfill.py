d87b8e50828852531b9db43ae2972bdaa7f4517d
Simon Fish <si@mon.fish>
2017-11-18 19:11:30 +0000

    Fill database with sweet sweet moonage

--- dbfill.py
+++ dbfill.py
@@ -1,7 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 db_connect = create_engine('sqlite:///moons.db')
 moonlist = open('moonlist', 'r')
+conn = db_connect.connect()
+statement = text("""INSERT INTO moons (kingdom, name, moon_type, is_postgame) VALUES (:kingdom, :name, :moon_type, :is_postgame)""") 
 for moon in moonlist:
-    print([text.strip() for text in moon.split('|')])
-    # conn = db_connect.connect()
-    # count = conn.execute("select Count(*) from moons").fetchone()[0]
+    moon_data = [text.strip() for text in moon.split('|')]
+    data = ( { "kingdom": moon_data[0], "name": moon_data[1], "moon_type": moon_data[2], "is_postgame": moon_data[3] } )
+    conn.execute(statement, **data)
+    print("Added", data['name'])
+
