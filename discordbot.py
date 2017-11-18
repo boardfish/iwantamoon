c4f230d24249095fc271bcbefbefcb8f4427eb45
Simon Fish <si@mon.fish>
2017-11-18 20:35:32 +0000

    Add extra details to Discord bot response...but I can't test it because it's down ngaaaaaaaaaah

--- discordbot.py
+++ discordbot.py
@@ -21,7 +21,7 @@ def gen_moon():
     moon_id = randint(1,count)
     query = conn.execute("select * from moons where id =%d "  %int(moon_id))
     result = query.fetchone()
-    template = "You got a moon! " + result['name']
+    template = "**Let's find a moon!** " + result['name'] + " You can find this moon in the " + result['kingdom'] + " Kingdom" + ("post-game.", ".")[[result['is_postgame']]]
     sfx = choose_sfx(result['moon_type'])
     return [template, sfx]
 
