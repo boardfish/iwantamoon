283279f0ae5130941e28568b0e27c2101f4c03fb
Simon Fish <si@mon.fish>
2017-11-18 13:35:43 +0000

    Implement Alexa skill

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import logging
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_ask import Ask, statement, question, session
 from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonify
@@ -9,7 +11,20 @@ import os
 
 db_connect = create_engine('sqlite:///moons.db')
 app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 api = Api(app)
+
+@ask.launch
+
+@ask.intent('MoonIntent')
+def moon():
+    conn = db_connect.connect()
+    count = conn.execute("select Count(*) from moons").fetchone()[0]
+    moon_id = randint(1,count)
+    query = conn.execute("select name from moons where id =%d "  %int(moon_id))
+    result = query.fetchone()[0]
+    return statement("You got a moon! " + result)
 
 class Moons(Resource):
     def get(self):
@@ -33,4 +48,4 @@ api.add_resource(RandomMoon, '/random') # Route_2
 
 
 if __name__ == '__main__':
-     app.run(port=os.getenv('PORT', '5002'), host='0.0.0.0')
+     app.run(port=os.getenv('PORT', '5000'), host='0.0.0.0')
